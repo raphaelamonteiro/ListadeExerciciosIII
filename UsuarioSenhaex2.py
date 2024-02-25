9143cea8ac61eff736eca084a392ee78f4f62224
Raphaela <125154913+raphaelamonteiro@users.noreply.github.com>
2024-02-25 16:23:25 -0300

    Add files via upload

---
+++ UsuarioSenhaex2.py
@@ -0,0 +1,8 @@
+username = str(input("nome de usuário: "))
+senha = str(input("senha: "))
+
+while (username) == (senha):
+    senha = str(input("Erro! Insira uma senha válida: "))
+
+
+print("senha válida")
